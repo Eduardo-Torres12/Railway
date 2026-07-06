f2f02320a45cd571207f6c6029b5424928e10a51
Eduardo-Torres12 <yoav.torres9839@alumnos.udg.mx>
2026-07-06 12:53:29 -0600

    Cambios en archivo obtener.php

--- obtener.php
+++ obtener.php
@@ -1,16 +1,21 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 include 'conexion.php';
 
 $sql = "SELECT * FROM usuarios";
 $resultado = $conexion->query($sql);
 
-$datos = array();
+if (!$resultado) {
+    die("Error en la consulta: " . $conexion->error);
+}
 
-while($fila = $resultado->fetch_assoc()){
+$datos = array();
+while ($fila = $resultado->fetch_assoc()) {
     $datos[] = $fila;
 }
 
-echo json_encode($datos);
-
+echo json_encode($datos, JSON_UNESCAPED_UNICODE);
 ?>
